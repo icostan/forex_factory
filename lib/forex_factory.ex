249d7fb1d4f69ecba2b9fee1ccea3d6b5154cb2e
Iulian Costan <git@costan.ro>
2024-03-20 11:02:34 +0200

    New project generation

---
+++ lib/forex_factory.ex
@@ -0,0 +1,18 @@
+defmodule ForexFactory do
+  @moduledoc """
+  Documentation for `ForexFactory`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ForexFactory.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/forex_factory_test.exs
@@ -0,0 +1,8 @@
+defmodule ForexFactoryTest do
+  use ExUnit.Case
+  doctest ForexFactory
+
+  test "greets the world" do
+    assert ForexFactory.hello() == :world
+  end
+end
